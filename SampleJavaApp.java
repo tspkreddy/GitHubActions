716dc262b4d6df05d03bba6b3aaf69ac381218dc
RupaMedapati <rupabhavanimedapati@gmail.com>
2024-06-10 11:45:08 +0530

    Rupa project

---
+++ SampleJavaApp.java
@@ -0,0 +1,23 @@
+import java.util.logging.Logger;
+
+public class SampleJavaApp {
+    private static final Logger logger = Logger.getLogger(SampleJavaApp.class.getName());
+
+    public static void main(String[] args) {
+        logger.info("Starting Sample Java Application");
+
+        SampleJavaApp app = new SampleJavaApp();
+        app.performOperation();
+
+        logger.info("Sample Java Application finished");
+    }
+
+    public void performOperation() {
+        logger.info("Performing operation...");
+        // Simulate some work
+        for (int i = 0; i < 5; i++) {
+            logger.info("Operation step " + i);
+        }
+        logger.info("Operation completed");
+    }
+}
